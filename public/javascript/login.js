acbbf2f1baf380b0799da1f32d094bd9f62bc5b7
Jared Davis <jared.davis21@yahoo.com>
2021-02-25 22:49:31 -0500

    using module 14 to create starting point

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -17,6 +17,7 @@ async function signupFormHandler(event) {
 
       if (response.ok) {
           console.log('success');
+          document.location.replace('/dashboard');
       } else {
           alert(response.statusText);
       }
@@ -29,7 +30,7 @@ async function loginFormHandler(event) {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && email && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
@@ -40,7 +41,7 @@ async function loginFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard/');
       } else {
         alert(response.statusText);
       }
